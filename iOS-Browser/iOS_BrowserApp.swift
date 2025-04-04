304e6e6f0db72f08fc117470a778d5d1ea8c18e3
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-04 11:42:27 +0530

    iOS-Browser

---
+++ iOS-Browser/iOS_BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_BrowserApp.swift
+//  iOS-Browser
+//
+//  Created by Krisna Pranav on 04/04/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
