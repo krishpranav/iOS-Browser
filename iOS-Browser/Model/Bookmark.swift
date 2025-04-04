c44736271b2ebdd3fd12774e03b9fa031b49eba6
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-04 11:50:02 +0530

    iOS-Browser: Model[Bookmark]

---
+++ iOS-Browser/Model/Bookmark.swift
@@ -0,0 +1,20 @@
+//
+//  Bookmark.swift
+//  iOS-Browser
+//
+//  Created by Krisna Pranav on 04/04/25.
+//
+
+import Foundation
+
+struct Bookmark: Identifiable, Codable {
+    let id: UUID
+    var title: String
+    var url: URL
+
+    init(id: UUID, title: String, url: URL) {
+        self.id = id
+        self.title = title
+        self.url = url
+    }
+}
