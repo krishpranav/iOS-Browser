1667ff3ba5a99404dd2fa1942a4a4d4bd4acd28b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-04-04 11:44:59 +0530

    iOS-Browser: Utilities[Theme]

--- iOS-Browser/Utilities/Theme.swift
+++ iOS-Browser/Utilities/Theme.swift
@@ -5,4 +5,21 @@
 //  Created by Krisna Pranav on 04/04/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct Theme {
+    static let primary = Color.yellow
+    static let secondary = Color.black
+    static let background = Color.black
+    static let text = Color.white
+    static let accent = Color.yellow
+
+    static func navigationBarAppearance() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor(Theme.background)
+        appearance.titleTextAttributes = [.foregroundColor: UIColor(Theme.text)]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor(Theme.text)]
+        
+    }
+}
